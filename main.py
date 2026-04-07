9e7ea39ad08db263b02b02267c5ad6b1acd89755
cam2104 <chv0805@gmail.com>
2026-04-07 16:29:40 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
-from carro import Carro
+from shampoo import Shampoo
 
 if __name__ == "__main__":
-    carro1 = Carro("Toyota", "Corolla", 80, 40, 60)
-    carro2 = Carro("Tesla", "Model S", 100, 0, 120)
-    carro3 = Carro("Ford", "Fiesta", 50, 30, 80)
-
-    print(carro1.arrancar())
-    print(carro2.arrancar())
-    print(carro3.arrancar())
-
-    print(carro1)
-    print(carro2)
-    print(carro3)
-
-    print("\nVelocidad actual:", carro1.velocidad)
-
-    carro1.velocidad = 90
-    print("Nueva velocidad:", carro1.velocidad)
-
-    carro1.velocidad = -10
+    shampooo1 = Shampoo("antiedad", "tio nacho", ["agua", "mentol","sulfato"],3,"botella",300,"vidrio",5.8)
+    shampoo2 = Shampoo("hidratante", "pantene", ["keratina","maicena","vitamina e","agua"],4,"dispensador",1000,"plastico",6.5)
+    shampoo3= Shampoo("suave para bebes", "arruru",["agua","gliceerina","manzanilla"],3,"botella",200,"plastico",6.8)
+    #polimorfismo
+    print(shampooo1.describir())
+    print(shampoo2.describir())
+    print(shampoo3.describir())
+    
+    #str de cada objeto
+    print(shampooo1)
+    print(shampoo2)
+    print(shampoo3)
+    #metodos propios de ingedientes y envase
+    print(shampooo1.infoingredientes())
+    print(shampoo2.infoenvase())
+    #encapsulamiento
+    print("ph actual de antiedad es",shampooo1.ph)
+    #modidicacion del ph con el setter
+    shampooo1.ph= 5.5
+    print ("Nuevo ph ", shampooo1.ph)
+    
+     #modicacion invalida
+    
+    shampooo1.ph=8.0
