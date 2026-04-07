181eee81819739cda7ba5492b5a18c8d3241efb0
cam2104 <chv0805@gmail.com>
2026-04-07 16:33:16 -0500

    Delete combustible.py

--- combustible.py
+++
@@ -1,9 +0,0 @@
-class Combustible:
-    def __init__(self, nivel_gasolina):
-        self.nivel_gasolina = nivel_gasolina
-
-    def repostar(self):
-        return f"Repostando gasolina: {self.nivel_gasolina} litros"
-if __name__ == "__main__":
-    c = Combustible(45)
-    print(c.repostar())
